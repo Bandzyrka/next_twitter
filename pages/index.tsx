8b235a6bd5d2d3842f954e9c030228ef35b13cf4
Wiktor Bandzyra <werstaptor@gmail.com>
2022-04-28 21:58:54 +0200

    Added next_auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,35 @@
 import Head from 'next/head'
 import Sidebar from '../components/sidebar/sidebar'
 import Feed from '../components/feed/feed'
-const Home = () => {
+import Login from '../components/login/login'
+
+import { getProviders, getSession, useSession } from "next-auth/react";
+
+
+export default function Home({providers}){
+  const { data: session } = useSession();
+  if(!session) return <Login providers={providers} />; 
   return (
     <div className="">
       <Head>
         <title>Next Twitter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
       <main className="bg-[#F5F8FA] flex min-h-screen max-w-[1500px] mx-auto">
         <Sidebar/>
         <Feed />
       </main>
-      
-    
     </div>
   )
 }
 
-export default Home
+export async function getServerSideProps(context: any){
+  const providers = await getProviders()
+  const session = await getSession()
+    return {
+      props: {
+        providers,
+        session,
+      }
+    }
+}
