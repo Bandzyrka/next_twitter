636afe8bd3fbc09cb22951cc1977d4384af70cb7
Wiktor Bandzyra <werstaptor@gmail.com>
2022-05-04 15:13:20 +0200

    Added liking functionality

--- components/post/post.jsx
+++ components/post/post.jsx
@@ -1,8 +1,48 @@
 import { ChatIcon, HeartIcon, SaveIcon, ShareIcon, DotsHorizontalIcon} from '@heroicons/react/outline'
-import React from 'react'
+import { HeartIcon as HeartIconFilled} from '@heroicons/react/solid';
+import { db } from '../../firebase';
+import React, {useState, useEffect} from 'react'
+import { getProviders, getSession, useSession } from "next-auth/react";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+} from "@firebase/firestore";
 
 const Post = ({post, id}) => {
   const {image, tag, text, timestamp, userImg, username} = post
+  const [isLiked, setIsLiked] = useState(false)
+  const [likes, setLikes] = useState([])
+  const { data: session } = useSession();
+
+  const likePost = async () => {
+    if(isLiked){
+      await deleteDoc(doc(db, "posts", id, "likes", session.user.uid))
+    } else{
+      await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+        name: session.user.name
+      })
+    }   
+  }
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+  useEffect(
+    () =>
+      setIsLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes]
+  );
+  
   return (
     <div className="flex flex-col w-full">
       <div className="flex w-full">
@@ -34,8 +74,22 @@ const Post = ({post, id}) => {
       </div>
       <div className="flex justify-between items-center p-4 ml-8 text-[#657786]">
           <ChatIcon className="h-7"/>
+          <div className="flex items-center justify-center">
+          {
+            isLiked ?
+              <HeartIconFilled 
+                className="h-7 text-pink-500 flip-in-hor-top"
+                onClick={likePost}
+              />
+              :
+              <HeartIcon 
+                className="h-7 hover:text-pink-500"
+                onClick={likePost}
+              />
+          }
+          <p className={`text-xl ${likes.length === 0 ? "text-white": "flex"}`}>{likes.length}</p>
+          </div>
           <ShareIcon className="h-7"/>
-          <HeartIcon className="h-7"/>
           <SaveIcon className="h-7"/>
       </div>
     </div>
